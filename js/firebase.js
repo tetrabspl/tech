d0f6f771893f1c2db3eeb0bf310ca3307366f68f
tetrabspl <tetrabspl@gmail.com>
2026-01-04 13:40:27 +0530

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,13 @@
-// Firebase App (the core Firebase SDK)
-const firebaseAppScript = document.createElement("script");
-firebaseAppScript.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js";
-document.head.appendChild(firebaseAppScript);
+// Load Firebase v8 SDKs
+var script1 = document.createElement("script");
+script1.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js";
+document.head.appendChild(script1);
 
-// Firebase Auth
-const firebaseAuthScript = document.createElement("script");
-firebaseAuthScript.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js";
-document.head.appendChild(firebaseAuthScript);
+var script2 = document.createElement("script");
+script2.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js";
+document.head.appendChild(script2);
 
-firebaseAuthScript.onload = function () {
+script2.onload = function () {
   var firebaseConfig = {
     apiKey: "AIzaSyA2-snzNhr5Cacpv6W63fe7RDN1WzzBBUo",
     authDomain: "tetratech-13e3e.firebaseapp.com",
