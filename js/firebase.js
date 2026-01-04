47b5f944658b0cf73706fc24c88ea2766571c7e3
tetrabspl <tetrabspl@gmail.com>
2026-01-04 13:26:13 +0530

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-znZNn5CacpV6N3F7RDNlVzzBbU",
+  apiKey: "AIzaSyA2-snzNhr5Cacpv6W63fe7RDN1WzzBBUo",
   authDomain: "tetratech-13e3e.firebaseapp.com",
   projectId: "tetratech-13e3e",
   storageBucket: "tetratech-13e3e.appspot.com",
   messagingSenderId: "774881231618",
-  appId: "1:774881231618:web:3899d93b28a45be09f8b1b"
+  appId: "1:774881231618:web:3899d93b28a45be09fb81b"
 };
 
 const app = initializeApp(firebaseConfig);
