b708d9ded06c1f7c08a71a78cba2e6f289a7af10
tetrabspl <tetrabspl@gmail.com>
2026-01-04 15:01:37 +0530

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,26 +1,12 @@
-// Load Firebase v8 SDKs
-var script1 = document.createElement("script");
-script1.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js";
-document.head.appendChild(script1);
-
-var script2 = document.createElement("script");
-script2.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js";
-document.head.appendChild(script2);
-
-script2.onload = function () {
-  var firebaseConfig = {
-    apiKey: "AIzaSyA2-snzNhr5Cacpv6W63fe7RDN1WzzBBUo",
-    authDomain: "tetratech-13e3e.firebaseapp.com",
-    projectId: "tetratech-13e3e",
-    storageBucket: "tetratech-13e3e.appspot.com",
-    messagingSenderId: "774881231618",
-    appId: "1:774881231618:web:3899d93b28a45be09fb81b"
-  };
-
-  firebase.initializeApp(firebaseConfig);
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA2-snzNhr5Cacpv6W63fe7RDN1WzzBBUo",
+  authDomain: "tetratech-13e3e.firebaseapp.com",
+  projectId: "tetratech-13e3e",
+  storageBucket: "tetratech-13e3e.appspot.com",
+  messagingSenderId: "774881231618",
+  appId: "1:774881231618:web:3899d93b28a45be09fb81b"
 };
-// Load Firestore
-var script3 = document.createElement("script");
-script3.src = "https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js";
-document.head.appendChild(script3);
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
